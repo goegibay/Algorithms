247a5eb3f032b3672cf6100649596aa47524026f
Steven Huang <stevenhuang42195@gmail.com>
2019-05-01 15:03:02 +0800

    test

--- scripts/readme.py
+++ scripts/readme.py
@@ -16,8 +16,8 @@ class Config:
     #local_path = '/Users/stevenhuang/Dropbox/Leetcode/Algorithms'
     local_path =  '/Users/Steven/Documents/Algorithms'
     # solution of leetcode
-    github_leetcode_url = 'https://github.com/goegibay/Algorithms'
-    #github_leetcode_url =  'https://github.com/goegibay/Algorithms/blob/master/leetcode-algorithms'
+    #github_leetcode_url = 'https://github.com/goegibay/Algorithms'
+    github_leetcode_url =  'https://github.com/goegibay/Algorithms/blob/master/leetcode-algorithms'
 
     # solution of pat,　暂时还没写
     github_pat_url = 'https://github.com/hey-bruce/algorithms_and_oj/blob/master/pat-algorithms/'
